536adcdcd37d6102c01338cf837c88c05b188764
Imma Lopez <immals27@gmail.com>
2021-10-17 22:12:35 +0200

    Filter out None in vocabulary

--- graded_readers_stats/utils.py
+++ graded_readers_stats/utils.py
@@ -6,7 +6,9 @@ def get_vocab_freq_for_level(vocab_item, level, vocab_counts_per_level):
     return level_counts[0] / level_counts[1]
 
 
-def vocab_item_to_key(vocab_item):
-    return '_'.join(vocab_item)
+def vocab_item_to_key(vocab_item: [str]):
+    sanitized = filter(lambda word: word is not None, vocab_item)
+    return '_'.join(sanitized)
 
 
+print(vocab_item_to_key([None, 'hello', None, 'world', None, '!', None]))
