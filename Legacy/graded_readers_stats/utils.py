b3d5fec8e540f6357091d1949a05c085fd4f68ac
Imma Lopez <immals27@gmail.com>
2021-11-02 11:23:47 +0100

    Delete Legacy folder containing first iteration code

--- Legacy/graded_readers_stats/utils.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-#                                  UTILITIES                                 #
-##############################################################################
-
-def vocab_item_to_key(vocab_item: [str]):
-    """Gets rid of 'None'-type items, so that vocabulary units containing them
-    can still be used as keys in a dictionary."""
-    sanitized = filter(lambda word: word is not None, vocab_item)
-    return '_'.join(sanitized)
