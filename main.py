34dd10ec91e3a657c87f9181ea62a38c0ece93ee
Imma Lopez <immals27@gmail.com>
2021-11-03 02:40:40 +0100

    Profile code execution

--- main.py
+++ main.py
@@ -6,29 +6,44 @@ from graded_readers_stats import data, frequency, preprocess, tree
 from graded_readers_stats.constants import *
 
 
-# Load data
-vocabulary, readers = data.load(trial=True)
+def main():
+    # Load data
+    vocabulary, readers = data.load(trial=True)
 
-# Preprocess data
-readers = preprocess.run(readers, preprocess.text_analysis_pipeline)
-vocabulary = preprocess.run(vocabulary, preprocess.vocabulary_pipeline)
-preprocess.collect_context_for_phrases_in_texts(
-    vocabulary, readers, column_prefix=PREFIX_READER
-)
-readers_by_level = readers.groupby(COL_LEVEL)
+    # Preprocess data
+    readers = preprocess.run(readers, preprocess.text_analysis_pipeline)
+    vocabulary = preprocess.run(vocabulary, preprocess.vocabulary_pipeline)
+    preprocess.collect_context_for_phrases_in_texts(
+        vocabulary, readers, column_prefix=PREFIX_READER
+    )
+    readers_by_level = readers.groupby(COL_LEVEL)
 
-# Vocabulary Frequencies
-frequency.count_vocab_in_sentences_by_groups(
-    vocabulary, readers_by_level, column_prefix=PREFIX_READER
-)
+    # Vocabulary Frequencies
+    frequency.count_vocab_in_sentences_by_groups(
+        vocabulary, readers_by_level, column_prefix=PREFIX_READER
+    )
 
-# Vocabulary's Context Frequencies
-frequency.count_vocab_context_in_sentences_by_groups(
-    vocabulary, readers_by_level, column_prefix=PREFIX_READER_CONTEXT
-)
+    # Vocabulary's Context Frequencies
+    frequency.count_vocab_context_in_sentences_by_groups(
+        vocabulary, readers_by_level, column_prefix=PREFIX_READER_CONTEXT
+    )
 
-# Tree widths and depths
-tree.get_tree_widths_and_depths(
-    vocabulary,
-    readers_by_level
-)
+    # Tree widths and depths
+    tree.get_tree_widths_and_depths(
+        vocabulary,
+        readers_by_level
+    )
+
+    print('DONE!')
+
+
+# if __name__ == '__main__':
+#     main()
+
+import profile
+profile.run('main()', 'profile_stats')
+
+import pstats
+from pstats import SortKey
+p = pstats.Stats('profile_stats')
+p.sort_stats(SortKey.CUMULATIVE).print_stats(20)
