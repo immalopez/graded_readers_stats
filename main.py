477358a82ae3c06be24e4101b098426198b3a8bb
Imma Lopez <immals27@gmail.com>
2022-02-06 20:05:58 +0100

    Replace manual timer with context manager

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import time
 
+from codetiming import Timer
 from pandas.core.common import flatten
 
 from graded_readers_stats import (
     context,
     data,
-    frequency,
-    preprocess,
     tree,
     utils,
 )
@@ -17,63 +16,95 @@ from graded_readers_stats.constants import (
     FREQUENCY,
     TFIDF,
 )
+from graded_readers_stats.frequency import (
+    tfidfs
+)
+from graded_readers_stats.preprocess import (
+    run,
+    vocabulary_pipeline,
+    text_analysis_pipeline,
+    find_term_locations_in_docs,
+)
+
+timer_text = '{name}: {:0.2f} seconds'
 
 start_main = time.time()
 trial = True
 use_cache = False
 
 # Load data
-start = time.time()
-vocab = data.load(data.Dataset.VOCABULARY, trial, use_cache)
-readers = data.load(data.Dataset.READERS, trial, use_cache)
-utils.duration(start, 'Loading')
+with Timer(name='Load data', text=timer_text):
+    vocab = data.load(data.Dataset.VOCABULARY, trial, use_cache)
+    readers = data.load(data.Dataset.READERS, trial, use_cache)
 
 # Group
-reader_by_level = readers.groupby(COL_LEVEL)
-group_1 = reader_by_level.get_group('Inicial').reset_index(drop=True)
+with Timer(name='Group', text=timer_text):
+    reader_by_level = readers.groupby(COL_LEVEL)
+    group_1 = reader_by_level.get_group('Inicial').reset_index(drop=True)
 
 # Preprocess
 # TODO: Move non-output columns to local var
-start = time.time()
-vocab = preprocess.run(vocab, preprocess.vocabulary_pipeline)
-group_1 = preprocess.run(group_1, preprocess.text_analysis_pipeline)
-utils.duration(start, 'Preprocess')
+with Timer(name='Preprocess', text=timer_text):
+    vocab = run(vocab, vocabulary_pipeline)
+    group_1 = run(group_1, text_analysis_pipeline)
 
-# Locate terms
-start = time.time()
-terms = [term for terms in vocab[COL_LEMMA] for term in terms]
-locations = preprocess.find_term_locations_in_docs(terms, group_1[COL_LEMMA])
-utils.duration(start, 'Find terms')
+##############################################################################
+#                               Process Terms                                #
+##############################################################################
 
-# Frequency
-start = time.time()
-found_words = [sum(1 for doc in docs for _ in doc) for docs in locations]
-total_words = sum(1 for _ in flatten(group_1[COL_LEMMA]))
-vocab[FREQUENCY] = [count / total_words for count in found_words]
-utils.duration(start, 'Frequency')
+with Timer(name='Locate terms', text=timer_text):
+    terms = [term for terms in vocab[COL_LEMMA] for term in terms]
+    terms_locs = find_term_locations_in_docs(terms, group_1[COL_LEMMA])
 
-# TFIDF
-start = time.time()
-vocab[TFIDF] = frequency.tfidfs(term_locs=locations, docs=group_1[COL_LEMMA])
-utils.duration(start, 'TFIDF')
+with Timer(name='Frequency', text=timer_text):
+    found_words = [sum(1 for doc in docs for _ in doc) for docs in terms_locs]
+    total_words = sum(1 for _ in flatten(group_1[COL_LEMMA]))
+    vocab[FREQUENCY] = [count / total_words for count in found_words]
 
-# Tree
-start = time.time()
-trees = tree.make_trees_for_occurrences_v2(
-    term_locs=locations, stanza_docs=group_1[COL_STANZA_DOC]
-)
-vocab['Trees'] = tree.calculate_tree_props_v2(trees)
-utils.duration(start, 'Trees')
+with Timer(name='TFIDF', text=timer_text):
+    vocab[TFIDF] = tfidfs(term_locs=terms_locs, docs=group_1[COL_LEMMA])
+
+with Timer(name='Trees', text=timer_text):
+    trees = tree.make_trees_for_occurrences_v2(
+        term_locs=terms_locs, stanza_docs=group_1[COL_STANZA_DOC]
+    )
+    vocab['Trees'] = tree.calculate_tree_props_v2(trees)
+
+
+##############################################################################
+#                                Contexts                                    #
+##############################################################################
 
 # Collect context
-context_words = context.collect_context_words_multiple(
-    locations, group_1[COL_LEMMA], window=3
+contexts_by_term = context.collect_context_words_multiple(
+    terms_locs, group_1[COL_LEMMA], window=3
 )
 
 # Locate context
+# - unified context list
+ctx_terms_flat = list(flatten(contexts_by_term))
+ctx_terms = [[word] for word in ctx_terms_flat]
+ctx_terms_locs = find_term_locations_in_docs(ctx_terms, group_1[COL_LEMMA])
+ctx_term_loc_map = dict(zip(ctx_terms_flat, ctx_terms_locs))
+
 # Frequency context
+# - select relevant words
+start = time.time()
+
+ctx_terms_2 = contexts_by_term[2]
+ctx_terms_2_locs = map(ctx_term_loc_map.__getitem__, ctx_terms_2)
+
+
+found_words = [sum(1 for doc in docs for _ in doc) for docs in ctx_terms_2_locs]
+total_words = sum(1 for _ in flatten(group_1[COL_LEMMA]))
+ctx_freqs = [count / total_words for count in found_words]
+utils.duration(start, 'Context Frequency')
+
 # TFIDF context
+# - select relevant words
+
 # Tree context
+# - select relevant words
 
 # MSTTR for all text
 
