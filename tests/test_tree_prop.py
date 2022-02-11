a99f73e93ec713073405f3c4aa91c743f9f3083f
Imma Lopez <immals27@gmail.com>
2022-02-11 05:59:08 +0100

    Reformat for readability

--- tests/test_tree_prop.py
+++ tests/test_tree_prop.py
@@ -17,95 +17,38 @@ def storage():
         'stanza': [  # list of docs
             DotDict({  # a doc
                 'sentences': [
-
-                    # Head
-                    #   > Word1
-                    #       > Word3
-                    #   > Word2
-                    #       > Word4
-                    #       > Word5
+                    # Head1
+                    # |-- Word2
+                    # |   +-- Word4
+                    # +-- Word3
+                    #     |-- Word5
+                    #     +-- Word6
                     DotDict({'words': [  # sentence a.k.a. a sequence of words
-                        DotDict({
-                            'id': 1,
-                            'lemma': 'Head',
-                            'head': 0
-                        }),
-                        DotDict({
-                            'id': 2,
-                            'lemma': 'Word1',
-                            'head': 1
-                        }),
-                        DotDict({
-                            'id': 4,
-                            'lemma': 'Word3',
-                            'head': 2
-                        }),
-                        DotDict({
-                            'id': 3,
-                            'lemma': 'Word2',
-                            'head': 1
-                        }),
-                        DotDict({
-                            'id': 5,
-                            'lemma': 'Word4',
-                            'head': 3
-                        }),
-                        DotDict({
-                            'id': 6,
-                            'lemma': 'Word5',
-                            'head': 3
-                        }),
+                        DotDict({'id': 1, 'lemma': 'Head1', 'head': 0}),
+                        DotDict({'id': 2, 'lemma': 'Word2', 'head': 1}),
+                        DotDict({'id': 4, 'lemma': 'Word4', 'head': 2}),
+                        DotDict({'id': 3, 'lemma': 'Word3', 'head': 1}),
+                        DotDict({'id': 5, 'lemma': 'Word5', 'head': 3}),
+                        DotDict({'id': 6, 'lemma': 'Word6', 'head': 3}),
                     ]}),
 
-                    # Head
-                    #   > Word1
-                    #       > Word3
-                    #       > Word6 NEW
-                    #   > Word2
-                    #       > Word4
-                    #       > Word5
-                    #          > Word7 NEW
+                    # Head1
+                    # |-- Word2
+                    # |   +-- Word4
+                    # |   +-- Word7 NEW
+                    # +-- Word3
+                    #     |-- Word5
+                    #     +-- Word6
+                    #         +-- Word8 NEW
                     DotDict({'words': [
-                        DotDict({
-                            'id': 1,
-                            'lemma': 'Head',
-                            'head': 0
-                        }),
-                        DotDict({
-                            'id': 2,
-                            'lemma': 'Word1',
-                            'head': 1
-                        }),
-                        DotDict({
-                            'id': 4,
-                            'lemma': 'Word3',
-                            'head': 2
-                        }),
-                        DotDict({
-                            'id': 7,
-                            'lemma': 'Word6',
-                            'head': 2
-                        }),
-                        DotDict({
-                            'id': 3,
-                            'lemma': 'Word2',
-                            'head': 1
-                        }),
-                        DotDict({
-                            'id': 5,
-                            'lemma': 'Word4',
-                            'head': 3
-                        }),
-                        DotDict({
-                            'id': 6,
-                            'lemma': 'Word5',
-                            'head': 3
-                        }),
-                        DotDict({
-                            'id': 8,
-                            'lemma': 'Word7',
-                            'head': 6
-                        }),
+                        DotDict({'id': 1, 'lemma': 'Head1', 'head': 0}),
+                        DotDict({'id': 2, 'lemma': 'Word2', 'head': 1}),
+                        DotDict({'id': 4, 'lemma': 'Word4', 'head': 2}),
+                        DotDict({'id': 7, 'lemma': 'Word7', 'head': 2}),
+                        DotDict({'id': 3, 'lemma': 'Word3', 'head': 1}),
+                        DotDict({'id': 5, 'lemma': 'Word5', 'head': 3}),
+                        DotDict({'id': 6, 'lemma': 'Word6', 'head': 3}),
+                        DotDict({'id': 8, 'lemma': 'Word8', 'head': 6}),
                     ]})
                 ]
             })
@@ -113,17 +56,16 @@ def storage():
     }
     return DotDict(storage)
 
-
 def test_make_tree_for_loc(storage):
     # When
     tree = make_tree_for_loc(storage, 0, 0)
 
     # Then
-    assert "Node('/Head')" == repr(tree)
-    assert "(Node('/Head/Word1'), Node('/Head/Word2'))" == repr(tree.children)
-    assert "(Node('/Head/Word1/Word3'), "\
-           "Node('/Head/Word2/Word4'), "\
-           "Node('/Head/Word2/Word5'))" == repr(tree.leaves)
+    assert "Node('/Head1')" == repr(tree)
+    assert "(Node('/Head1/Word2'), Node('/Head1/Word3'))" == repr(tree.children)
+    assert "(Node('/Head1/Word2/Word4'), "\
+           "Node('/Head1/Word3/Word5'), "\
+           "Node('/Head1/Word3/Word6'))" == repr(tree.leaves)
 
 
 def test_get_tree_props(storage):
