e997841b3805eafa5f781ec5e8ed03b241f966c3
Imma Lopez <immals27@gmail.com>
2022-02-13 01:00:33 +0100

    Do capture stderr in logs

--- scripts/analyze_trial.sh
+++ scripts/analyze_trial.sh
@@ -1,8 +1,8 @@
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Inicial 2>&1 | tee output/Inicial.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Intermedio 2>&1 | tee output/Intermedio.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Avanzado 2>&1 | tee output/Avanzado.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Infantil 2>&1 | tee output/Infantil.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Juvenil 2>&1 | tee output/Juvenil.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Adulta 2>&1 | tee output/Adulta.log &
-venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Native list.csv" Native 2>&1 | tee output/Native.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Inicial | tee output/Inicial.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Intermedio | tee output/Intermedio.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Graded readers list.csv" Avanzado | tee output/Avanzado.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Infantil | tee output/Infantil.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Juvenil | tee output/Juvenil.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Literature list.csv" Adulta | tee output/Adulta.log &
+venv/bin/python main.py analyze "Data_trial/Vocabulary list.csv" "Data_trial/Native list.csv" Native | tee output/Native.log &
 wait
